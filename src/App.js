4f24b163c445d3681bc3bdf5e35b35aa542696e7
@douglasabnovato <douglasabnovato@gmail.com>
2019-11-05 18:35:43 -0300

    buscando na API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 
+
 import Header from "./components/Header";
+import Main from "./pages/main"
 
 import './styles/styles.css';
 
 const App = () => (
     <div className="App">
         <Header />
+        <Main />
     </div>
 );
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
-import axios from "axios";
-import { getToken } from "./auth";
+import axios from "axios"; 
 
-const api = axios.create({baseURL: ""});
-
+const api = axios.create({baseURL: "https://swapi.co/api/people"});
+//https://swapi.co/api/
+/*
 api.interceptors.request.use(async config => {
   const token = getToken();
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
-});
+});*/
 
 export default api;
